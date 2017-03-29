78049856d99226ed57f93287a084f228b2c26a21
bdjones2410 <brandon.jones@blackbaud.com>
2017-03-29 14:29:44 -0400

    About page tests pass

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,5 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html'
 })
 export class AboutComponent {
+  public aboutSections = [
+    {
+      sectionTitle: 'Menu component',
+      sectionDescription: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Sed maximus molestie aliquam. Vestibulum scelerisque auctor efficitur. 
+        Etiam pulvinar rutrum magna at rutrum.
+        `
+    },
+    {
+      sectionTitle: 'Getting started',
+      sectionDescription: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Sed maximus molestie aliquam. Vestibulum scelerisque auctor efficitur. 
+        Etiam pulvinar rutrum magna at rutrum.
+        `
+    },
+    {
+      sectionTitle: 'Installation instructions',
+      sectionDescription: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Sed maximus molestie aliquam. Vestibulum scelerisque auctor efficitur. 
+        Etiam pulvinar rutrum magna at rutrum.
+        `
+    }
+  ];
   constructor() {}
 }
