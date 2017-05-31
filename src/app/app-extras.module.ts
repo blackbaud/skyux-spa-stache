362cf1d72ee83d9a7c9760d291e62a7ae2cb0fc1
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-05-31 12:30:35 -0400

    Removed bootstrapper

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 
-import { BBAuth } from '@blackbaud/auth-client';
-
 import { SkyAppConfig } from '@blackbaud/skyux-builder/runtime';
 
-import { SkyAppBootstrapper } from '@blackbaud/skyux-builder/runtime/bootstrapper';
-
 import { StacheModule, StacheConfigService } from '@blackbaud/stache';
-
-const decode = require('jwt-decode');
 
 // Specify entry components, module-level providers, etc. here.
 @NgModule({
@@ -27,24 +21,3 @@ const decode = require('jwt-decode');
   entryComponents: []
 })
 export class AppExtrasModule { }
-
-(SkyAppBootstrapper as any).processBootstrapConfig = () => {
-  return BBAuth.getToken().then((token: string) => {
-    const emailDomainWhitelist: any = [
-      'blackbaud.com',
-      'blackbaud.me',
-      'blackbaud.co.uk',
-      'blackbaud.au',
-      'microedge.com',
-      'attentive.ly',
-      'everydayhero.com'
-    ];
-    let parsedToken = decode(token);
-    let domain = parsedToken.email.split('@')[1];
-    
-    if (emailDomainWhitelist.includes(domain)) {
-      return Promise.resolve(true);
-    }
-    return Promise.reject('Must be a Blackbaud employee to access this content. Please log in with a valid Blackbaud email address.');
-  });
-};
