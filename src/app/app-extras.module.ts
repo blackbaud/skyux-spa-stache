be5148b0692f4f0ba2d098fa9e7e49918814ed77
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-06-13 14:54:16 -0400

    Removed stache extras

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import { StacheModule } from '@blackbaud/stache';
-import { StacheExtrasModule } from './stache-extras.module';
 
 // Specify entry components, module-level providers, etc. here.
 @NgModule({
   imports: [
-    StacheModule,
-    StacheExtrasModule
+    StacheModule
   ],
   exports: [
-    StacheModule,
-    StacheExtrasModule
+    StacheModule
   ],
   providers: [],
   entryComponents: []
--- src/app/stache-extras.module.ts
+++
@@ -1,4 +0,0 @@
-import { NgModule } from '@angular/core';
-export let STACHE_EXTRAS_PROVIDERS: any[] = [];
-@NgModule({})
-export class StacheExtrasModule { }
